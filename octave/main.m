d06421b040ffe9ab0bd0f9265b4e2efeb81c2ac1
Eugen Morozov <meugenom@gmail.com>
2026-03-13 16:12:06 +0100

    project structure

---
+++ octave/main.m
@@ -0,0 +1,8 @@
+% ============================================
+% main.m
+% Start point for calculation of SIZE and LUT for portable ring buffer
+% Method: Leaky Bucket + Cases
+% Output: file: includes/ring_buffer_config.h
+% Source:
+% meugenom 2026 
+% ============================================
